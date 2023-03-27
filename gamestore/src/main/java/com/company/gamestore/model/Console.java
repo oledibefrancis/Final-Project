61f52ff14120a4362e3d6d6300df963df1ed65d4
Obaloluwa123 <oolaniran3586@myasu.alasu.edu>
2023-03-27 18:53:47 -0500

    Added the models

---
+++ gamestore/src/main/java/com/company/gamestore/model/Console.java
@@ -0,0 +1,99 @@
+package com.company.gamestore.model;
+
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Console {
+
+    @Id
+    private int id;
+    private String model;
+    private String manufacturer;
+    private String memory_amount;
+    private String processor;
+    private BigDecimal price;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getMemory_amount() {
+        return memory_amount;
+    }
+
+    public void setMemory_amount(String memory_amount) {
+        this.memory_amount = memory_amount;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Console console = (Console) o;
+        return id == console.id && quantity == console.quantity && Objects.equals(model, console.model) && Objects.equals(manufacturer, console.manufacturer) && Objects.equals(memory_amount, console.memory_amount) && Objects.equals(processor, console.processor) && Objects.equals(price, console.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, manufacturer, memory_amount, processor, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Console{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", memory_amount='" + memory_amount + '\'' +
+                ", processor='" + processor + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ gamestore/src/main/java/com/company/gamestore/model/T_Shirt.java
@@ -0,0 +1,89 @@
+package com.company.gamestore.model;
+
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class T_Shirt {
+
+    @Id
+    private int id;
+    private String size;
+    private String color;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        T_Shirt t_shirt = (T_Shirt) o;
+        return id == t_shirt.id && quantity == t_shirt.quantity && Objects.equals(size, t_shirt.size) && Objects.equals(color, t_shirt.color) && Objects.equals(description, t_shirt.description) && Objects.equals(price, t_shirt.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, size, color, description, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "T_Shirt{" +
+                "id=" + id +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
