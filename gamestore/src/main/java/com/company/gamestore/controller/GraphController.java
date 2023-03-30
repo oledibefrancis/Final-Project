3de89c969efdeea5def1e5ff464983ffdec64d3f
Ibrahim Awoleiro <ibrahimawoleiro@Ibrahims-MacBook-Pro.local>
2023-03-30 18:03:18 -0400

    Updating graphql

--- gamestore/src/main/java/com/company/gamestore/controller/GraphController.java
+++ gamestore/src/main/java/com/company/gamestore/controller/GraphController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-
 import java.util.List;
 @Controller
 public class GraphController {
