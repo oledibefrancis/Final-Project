6830995081afd9be6709bf66b6121e842600e8ce
oledibefrancis <somtofrancis380@gmail.com>
2023-04-05 15:59:47 -0500

    Implemented and tested fee and tax repositories

---
+++ gamestore/src/main/java/com/company/gamestore/repository/TaxRepository.java
@@ -0,0 +1,10 @@
+package com.company.gamestore.repository;
+
+import com.company.gamestore.model.Tax;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaxRepository extends JpaRepository<Tax,String> {
+
+}
